bcda3369764f5a8288ac4a891d399df8681ea425
degoeym <degoeym@gmail.com>
2014-09-19 23:15:41 -0400

    After some RSpec examples

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -39,4 +39,7 @@ RSpec.configure do |config|
   # the seed, which is printed after each run.
   #     --seed 1234
   config.order = "random"
+
+  # Added as part of Rails Tutorial instructions
+  config.include Capybara::DSL
 end
